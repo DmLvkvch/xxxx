cc7ef473cd9976cdc47db85f3bbe97ab4b22740c
DmLvkvch <dima2231298@gmail.com>
2020-02-18 11:47:29 +0300

    Java&Script task: refactoring

---
+++ refactoring/Finder.java
@@ -0,0 +1,33 @@
+﻿package ru.esphere.school.service;
+
+import ru.esphere.school.data.Member;
+import ru.esphere.school.data.MembersGroup;
+
+import java.util.Set;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Finder {
+    /**
+     * Поиск групп людей старше определенного возраста.
+     *
+     * @return список имен групп из списка групп старше возраста targetAge
+     */
+
+    /**
+     * Поиск групп людей старше определенного возраста.
+     *
+     * @param groups группы
+     * @param targetAge возраст для поиска
+     * @return список имен групп из списка групп старше возраста targetAge
+     */
+    public static Set<String> findOldMembers(List<MembersGroup> groups, int targetAge) {
+        Set<String> groupsNames = groups.stream().
+                map(membersGroup -> membersGroup.getMembers()).
+                flatMap(members -> members.stream()).
+                filter(member -> member.getAge() > targetAge).
+                map(Member::getName).collect(Collectors.toSet());
+
+        return groupsNames;
+    }
+}
